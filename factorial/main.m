688f02601563c36e0ff2f7b112035c3c29f040b3
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-14 16:50:58 +0530

    factorial

--- factorial/main.m
+++ factorial/main.m
@@ -13,9 +13,19 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
+        int i,fact=1,n;
+        NSLog(@"enter the number");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+        fact=fact*i;
+            
+            
+        }
+        NSLog(@"the factorial is %d",fact);
+
         // insert code here...
-        NSLog(@"Hello, World!");
-        
+       
     }
     return 0;
 }
